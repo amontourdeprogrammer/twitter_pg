8b07602408811d52293ab04eca43103583851532
meo84 <oopsiday@gmail.com>
2017-05-03 10:46:04 +0200

    setup environment variable for db name

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var pg = require('pg');
 require('dotenv').config();
 
-var database = "pg://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@localhost:5432/admin";
+var database = "pg://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@localhost:5432/" + process.env.DB_NAME;
 
 var client = new pg.Client(database);
 client.connect();
 
-var query = client.query("SELECT * FROM twitter_db.tweets ORDER BY date");
+var query = client.query("SELECT * FROM tweets ORDER BY date");
 query.on("row", function(row, result) {
   result.addRow(row);
 });
