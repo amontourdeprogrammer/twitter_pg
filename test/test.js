ba3f7ee806ce518e0747cf8ab296c3b891ac30c2
meo84 <oopsiday@gmail.com>
2017-05-08 11:11:51 +0200

    Add README

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var { read, insert, remove } = require('../db_manager');
 describe('db_manager', function() {
 
   before(function() {
+    table = 'tweets';
     tweet = {
       'message': 'A new tweet',
       'author': 'Noobie'
@@ -12,20 +13,20 @@ describe('db_manager', function() {
   });
 
   it('writes pg query to insert tweet', function() {
-    expect(insert('tweets', tweet)).to.eq('INSERT INTO tweets (message, author, date) ' +
+    expect(insert(table, tweet)).to.eq('INSERT INTO tweets (message, author, date) ' +
                                            'VALUES (\'A new tweet\', \'Noobie\', now())');
   });
 
   it('writes pg query to remove tweet', function() {
-    expect(remove('tweets', selection)).to.eq('DELETE FROM tweets WHERE author = \'Noobie\'');
+    expect(remove(table, selection)).to.eq('DELETE FROM tweets WHERE author = \'Noobie\'');
   });
 
   it('writes pg query to read all tweets', function() {
-    expect(read('tweets', 'all')).to.eq('SELECT * FROM tweets ');
+    expect(read(table, 'all')).to.eq('SELECT * FROM tweets ');
   });
 
   it('writes pg query to read specific tweets', function() {
-    expect(read('tweets', selection)).to.eq('SELECT * FROM tweets WHERE author = \'Noobie\'');
+    expect(read(table, selection)).to.eq('SELECT * FROM tweets WHERE author = \'Noobie\'');
   });
 
 });
